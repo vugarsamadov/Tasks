8800df09f403d7c4a695dab9d5d2b59d171e6eea
Vugar Samadov <mi779pf7t@code.edu.az>
2023-10-10 20:06:15 +0400

    Arrays task done.

---
+++ ArrayTasksWeek2/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] array = {0,1,2,3, 4, 5,6,7,8}; // Array with odd length
+//int[] array = {0,1,2,3,4, 5,6,7,8,9}; // Array with even length
+// int[] array = {0,1};
+// int[] array = {1};
+
+#region PRINTSTATEMENTS
+Console.Write("Array: ");
+for(int i = 0; i< array.Length; i++)
+    Console.Write(array[i]+" ");
+Console.WriteLine();
+#endregion
+
+int temp = 0;
+for(int i = 0; i< array.Length/2; i++) // O(n) in-place 
+{
+    temp = array[i];
+    array[i] = array[array.Length-1-i];
+    array[array.Length-1-i] = temp;
+}
+
+#region PRINTSTATEMENTS
+Console.Write("Reversed array: ");
+for(int i = 0; i < array.Length; i++)
+    Console.Write(array[i]+" ");
+#endregion
