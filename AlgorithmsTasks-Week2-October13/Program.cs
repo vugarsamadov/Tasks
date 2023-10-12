a2b9abf19ab2c43151c901033dab472b7ea5ca26
Vugar Samadov <mi779pf7t@code.edu.az>
2023-10-12 20:54:33 +0400

    Finished Algorithms Week 2 tasks due on Oct 13

---
+++ AlgorithmsTasks-Week2-October13/Program.cs
@@ -0,0 +1,27 @@
+﻿// 1.  Verilən ədədə və ya ondan böyük ən yaxın ədədə qədər fibonaççi ardıcıllığını ekrana çap edin.
+
+Console.Write("Eded daxil edin: ");
+int eded = Convert.ToInt32(Console.ReadLine());
+
+int head = 1;
+int tail = -1; 
+int temp;
+do
+{
+	Console.Write(head + tail + " ");
+	temp = head;
+	head = head + tail;
+	tail = temp;
+}while (eded > head);
+
+// 2. 2 dəyişənin dəyərlərini 3cü dəyişən istifadə etmədən dəyişin.
+
+int a = 6;
+int b = 8;
+
+a = a+b;
+b = a-b;
+a = a-b;
+
+
+
